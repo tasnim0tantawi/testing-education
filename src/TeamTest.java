eccd03b7ccaa3a024a62e4e1007d33964fb55127
tasnim0tantawi <tasnim0tantawi@gmail.com>
2022-12-20 23:12:57 +0300

    added test file

---
+++ src/TeamTest.java
@@ -0,0 +1,30 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import static org.junit.Assert.*;
+
+
+public class TeamTest {
+    public TeamTest() {
+    }
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    @Before
+    public void setUp() {
+    }
+    @After
+    public void tearDown() {
+    }
+    @Test
+
+
+
+
+}
